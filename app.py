f23dc98cf2f64827e635ecae3705842833e7c689
gopps95 <shankargopi7@gmail.com>
2024-03-20 22:02:11 +0530

    styling added

--- app.py
+++ app.py
@@ -1,23 +1,53 @@
 import streamlit as st
 from hugchat import hugchat
+streamlit_style = """
+			<style>
+			  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100&display=swap');
+
+			  .hotel-bold {
+			    font-weight: 600;
+			  }
+
+			  .hotel-font {
+			    font-size: 20px;
+          background-color: #e6f9ff;
+			  }
+
+			  label.css-1p2iens.effi0qh3{
+			    font-size: 18px;
+			  }
+
+			  p{
+			    font-size: 18px;
+			  }
+        li{
+          font-size: 18px;
+        }		
+        #MainMenu{
+        visibility: hidden;
+        }	  
+        button.css-135zi6y.edgvbvh9{
+        font-size: 18px;
+        font-weight: 600;
+        }
+			  
+			</style>
+			"""
 
 st.set_page_config(page_title="DialogDroid")
 
-# Generate empty lists for bot_response and user_input.
-## bot_response stores AI generated responses
+
 if 'bot_response' not in st.session_state:
     st.session_state['bot_response'] = ["I'm Sunny kuttan, How may I help you?"]
-## user_input stores User's questions
+
 if 'user_input' not in st.session_state:
     st.session_state['user_input'] = ['Hi!']
 
-# User input
 def get_input():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
 user_input = get_input()
 
-# Response output
 def generate_response(prompt):
     chatbot = hugchat.ChatBot(cookie_path="cookies.json")
     response = chatbot.chat(prompt)
